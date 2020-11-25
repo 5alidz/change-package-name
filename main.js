468399381cc5bb6b096774e1273237715234718e
khaled <kzack-dev@hotmail.com>
2020-11-25 19:12:43 +0300

    handle errors

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 #! /usr/bin/node
 const fs = require('fs');
-const newName = process.argv[2];
+const packageName = process.argv[2];
+const packagePath = './package.json';
 
-const package = JSON.parse(fs.readFileSync('./package.json').toString());
+function safeParseJson(str) {
+  try {
+    const maybeJson = JSON.parse(str);
+    return maybeJson != null && typeof maybeJson == 'object' ? maybeJson : {};
+  } catch (e) {
+    return {};
+  }
+}
 
-if (newName) {
-  package.name = newName;
+try {
+  const packageExist = fs.existsSync(packagePath);
+
+  if (!packageName) return console.log('[change-package-name] no name provided -- aborted');
+  if (!packageExist) return console.log('[change-package-name] no package.json was found -- aborted');
+
+  const package = safeParseJson(fs.readFileSync(packagePath).toString());
+  package.name = packageName;
   fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
+} catch (e) {
+  console.log(e.message);
 }
